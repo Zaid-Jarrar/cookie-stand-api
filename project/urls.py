d1988a7f3e3285c0071fbb7efa4325692414ae39
Zaid-Jarrar <zaid_jarrar@outlook.com>
2022-05-31 21:11:09 +0300

    deploying to heroku

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt import views as jwt_views
 
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/cookie-stands/", include("cookie_stands.urls")),
